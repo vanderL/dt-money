ebaa9cec701d05043b8b9db2119a0d3e4f54557c
vanderL <vnd.vander@gmail.com>
2021-09-20 17:13:53 -0300

    Component: Summary

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Sumary } from "../Sumary"
+
+import { Container } from "./styles"
+
+export const Dashboard = () => {
+    return(
+        <Container>
+            <Sumary />
+        </Container>
+    )
+}
