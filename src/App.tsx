e66ee52cdfc380ca88fbcb74cda2617dffce66c0
vanderL <vnd.vander@gmail.com>
2021-09-23 15:41:27 -0300

    Loading Transactions with React Context API

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./TransactionsContext";
 
 export function App() {
   const [isNewTransactionModalOperan, SetIsNewTransactionModal] = useState(false);
@@ -15,7 +16,7 @@ export function App() {
       SetIsNewTransactionModal(false);
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
       <NewTransactionModal 
@@ -23,6 +24,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
